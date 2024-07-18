a78fab3e320ff674f67eba1d94ae67685cb2c23f
Rodrigo Colque Garcia <colqueariel023@gmail.com>
2024-07-18 09:10:18 -0400

    feat: add function limiarData

--- js/calculadora_10Minds/main.js
+++ js/calculadora_10Minds/main.js
@@ -11,24 +11,62 @@ function selectButton(data) {
   const endPos = $dataInput.selectionEnd;
   const inputValue = $dataInput.value;
 
-  if (data === "c") {
+  if (data === "=") {
+    limpiarData(inputValue);
+  } else if (data === "c") {
     deleteAtCursor($dataInput, startPos, endPos, inputValue);
   } else if (data !== "0" || (inputValue.length > 0 && startPos > 0)) {
-    // Insertar el número en la posición correcta
-    const before = inputValue.substring(0, startPos);
-    const after = inputValue.substring(endPos, inputValue.length);
-    $dataInput.value = before + data + after;
-
-    // Mover el cursor después del número insertado
-    const newPos = startPos + data.length;
-    $dataInput.setSelectionRange(newPos, newPos);
-  } else {
-    console.log("Caso especial");
+    insertarNumero($dataInput, startPos, endPos, inputValue, data);
   }
-
   $dataInput.focus();
 }
 
+function insertarNumero($dataInput, startPos, endPos, inputValue, data) {
+  // Insertar el número en la posición correcta
+  const before = inputValue.substring(0, startPos);
+  const after = inputValue.substring(endPos, inputValue.length);
+  $dataInput.value = before + data + after;
+
+  // Mover el cursor después del número insertado
+  const newPos = startPos + data.length;
+  $dataInput.setSelectionRange(newPos, newPos);
+}
+
+function limpiarData(inputValue) {
+  if (
+    inputValue[inputValue.length - 1] !== "+" &&
+    inputValue[inputValue.length - 1] !== "-" &&
+    inputValue[inputValue.length - 1] !== "x" &&
+    inputValue[inputValue.length - 1] !== "/" &&
+    inputValue[inputValue.length - 1] !== "." &&
+    inputValue[inputValue.length - 1] !== "%" &&
+    inputValue[0] !== "x" &&
+    inputValue[0] !== "/"
+  ) {
+    for (let i = 0; i < inputValue.length; i++) {
+      if (inputValue[i] === "x") {
+        inputValue = inputValue.replace("x", "*");
+      }
+    }
+    try {
+      // Crear una nueva función que evalúa la expresión y retorna el resultado
+      const result = new Function(`return ${inputValue}`)();
+
+      // Verificar si el resultado es un número
+      if (isNaN(result)) {
+        throw new Error("La operación no se puede realizar.");
+      }
+
+      console.log(result);
+    } catch (error) {
+      console.log("Expresion NO valida");
+    }
+  } else {
+    console.log("No se puede calcular con un operador al final");
+  }
+}
+
+// evitamos que se pueda escribir en el input desde el teclado
 document.addEventListener("DOMContentLoaded", (event) => {
   const $dataInput = document.getElementById("inputData");
   $dataInput.focus();
@@ -39,6 +77,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
+// Función para borrar el contenido del input en la posición del cursor
 function deleteAtCursor($dataInput, startPos, endPos, inputValue) {
   let before;
   let after;
